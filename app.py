3414f13b17508d265630e3150ee6faf5076151ad
Tim Basten <tbasten@gmail.com>
2024-09-07 21:25:25 +0800

    pandas data manipulation

--- app.py
+++ app.py
@@ -10,6 +10,23 @@ app = flask.Flask(__name__, static_url_path='',
             template_folder='templates')
 app.config["DEBUG"] = True
 
+sensor_map = {
+    "Mary Cairncross Scenic Reserve": {
+        "BBQ_East": {
+            "temp": [
+                "r718b140-06f8"
+                "r718b140-06fc"
+                "r718b140-06f9"
+            ]
+        },
+        "BBQ_West": {
+            "temp": [
+                "r718b140-06f7"
+                "r718b140-06fa"
+            ]
+        }
+    }
+}
 
 
 @app.route("/")
@@ -20,29 +37,9 @@ def admin():
     return redirect(url_for("home"))
 
 df = pd.read_csv('data/r718b140-06f7-BBQ Temp.csv')
-grouped_data = df.groupby('dev_id')
 
-def create_nested_structure(group):
-    nested_dict = defaultdict(dict)
-    
-    for _, row in group.iterrows():
-        dt_key = row['datetime']
-        nested_dict[row['dev_id']][dt_key] = {
-            'battery': row['battery'],
-            'batteryDiff': row['batteryDiff'],
-            'temperature': row['temperature'],
-            'temperatureDiff': row['temperatureDiff'],
-        }
-    
-    return dict(nested_dict)
-
-result = {k: v for k, v in grouped_data.apply(create_nested_structure).items()}
-
-for dev_id, data in result.items():
-    sorted_data = dict(sorted(data.items(), key=lambda x: x[0]))
-    result[dev_id] = sorted_data
-
-json_output = json.dumps(result, indent=2)
+for i in df:
+    print(i)
 
 if __name__ == '__main__':
     app.run(host="localhost", port=8000, debug=True)
