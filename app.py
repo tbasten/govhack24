60a60b1c7d617eb0eff7a9584d20f0c15abe3267
Tim Basten <tbasten@gmail.com>
2024-09-08 14:21:30 +0800

    logos

--- app.py
+++ app.py
@@ -101,4 +101,8 @@ def admin():
 
 
 if __name__ == '__main__':
+<<<<<<< HEAD
   app.run(host='0.0.0.0', port=5000)
+=======
+    app.run(host="localhost", port=5000, debug=True)
+>>>>>>> a1a8fac (logos)
